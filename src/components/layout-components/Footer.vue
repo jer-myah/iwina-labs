50116396005118d204755a256164fa66d6494519
jer-myah <aluefuaehiabhi@gmail.com>
2021-08-23 14:41:24 +0100

    landing page

---
+++ src/components/layout-components/Footer.vue
@@ -0,0 +1,107 @@
+<template>
+    <footer class="w-full h-64 flex   items-center justify-center bg-black">
+        <div class="md:w-2/3 w-full px-4 text-white flex flex-col">
+            <div class="w-full text-4xl font-bold">
+                <h1 class="w-full md:w-2/3">How can we help you. get
+                    in touch</h1>
+            </div>
+            <div class="flex mt-8 flex-col md:flex-row md:justify-between">
+                <p class="w-full md:w-2/3 text-gray-400">To ensure that all Wikipedia content is verifiable, anyone may question an uncited claim. If your work has been tagged</p>
+                <div class="w-44 pt-6 md:pt-0">
+                    <a class="bg-red-500 justify-center text-center rounded-lg shadow px-10 py-3 flex items-center">Contact US</a>
+                </div>
+            </div>
+        </div>
+        
+    </footer>
+    <footer class="bg-black">
+        <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
+            <!-- Col-1 -->
+            <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+                <!-- Col Title -->
+                <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                    Iwina Labs
+                </div>
+    
+                <!-- Links -->
+                
+                Logo Here
+            </div>
+    
+            <!-- Col-2 -->
+            <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+                <!-- Col Title -->
+                <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                    Organization
+                </div>
+    
+                <!-- Links -->
+                <a href="" target="_blank" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                    About us
+                </a>
+                <a href="" target="_blank" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                    Our Team
+                </a>
+                <a href="" target="_blank" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                    Our Research
+                </a>
+            </div>
+    
+            <!-- Col-3 -->
+            <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+                <!-- Col Title -->
+                <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                    Content
+                </div>
+    
+                <!-- Links -->
+                <a href="" target="_blank" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                    Privacy Policy
+                </a>
+                <a href="" target="_blank" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                    Terms & Conditions
+                </a>
+            </div>
+    
+            <!-- Col-3 -->
+            <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+                <!-- Col Title -->
+                <div class="text-xs uppercase text-gray-400 font-medium mb-6">
+                    Community
+                </div>
+    
+                <!-- Links -->
+                <a href="" target="_blank" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                    Facebook
+                </a>
+                <a href="" target="_blank" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                    Whatsapp
+                </a>
+                <a href="" target="_blank" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                    Twitter
+                </a>
+                <a href="" target="_blank" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
+                    YouTube
+                </a>
+            </div>
+        </div>
+    
+        <!-- Copyright Bar -->
+        <div class="pt-2">
+            <div class="flex pb-5 px-3 m-auto pt-5 
+                border-t border-gray-500 text-gray-400 text-sm 
+                flex-col md:flex-row max-w-6xl">
+                <div class="mt-2">
+                    © Copyright 2021. All Rights Reserved.
+                </div>
+    
+                <!-- Required social icons (if you want) -->
+                <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+                    <a href="" target="_blank" class="w-6 mx-1">
+                        <i class=""></i>
+                    </a>                    
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
---
+++ src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+    <main-layout>
+        
+    </main-layout>
+</template>
+
+<script>
+import MainLayout from "../layouts/MainLayout.vue";
+
+export default {
+  name: 'Index',
+  components: {
+      MainLayout,
+  },
+  props: {
+
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
