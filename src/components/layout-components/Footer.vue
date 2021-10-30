996b42558435d60d43f879d600cb8d7554ad8e88
jer-myah <aluefuaehiabhi@gmail.com>
2021-10-30 16:49:12 +0100

    initial for iwina lab

--- src/components/layout-components/Footer.vue
+++ src/components/layout-components/Footer.vue
@@ -2,11 +2,10 @@
     <footer class="w-full h-64 flex   items-center justify-center bg-black">
         <div class="md:w-2/3 w-full px-4 text-white flex flex-col">
             <div class="w-full text-4xl font-bold">
-                <h1 class="w-full md:w-2/3">How can we help you. get
-                    in touch</h1>
+                <h1 class="w-full md:w-2/3">Get in touch with us.</h1>
             </div>
             <div class="flex mt-8 flex-col md:flex-row md:justify-between">
-                <p class="w-full md:w-2/3 text-gray-400">To ensure that all Wikipedia content is verifiable, anyone may question an uncited claim. If your work has been tagged</p>
+                <p class="w-full md:w-2/3 text-gray-400">Get in touch with us for collaboration.</p>
                 <div class="w-44 pt-6 md:pt-0">
                     <a class="bg-red-500 justify-center text-center rounded-lg shadow px-10 py-3 flex items-center">Contact US</a>
                 </div>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,39 @@
 <template>
     <main-layout>
-        
+        <div class="grid gap-4 sm:grid-cols-2">
+            <div class="col-span-2 text-center p-8 sm:p-16 ">
+                <h2 class="text-xl sm:text-5xl font-bold text-blue-400">
+                    Our <span class="text-gray-600">Focus</span>
+                </h2>
+                <p class="mt-3 text-lg text-gray-500">
+                    Our long-term determination is the advancement of innovative standards in our communities, through creation
+                    of cutting-edge systems and design of modern techniques to solve prevalent social problems.
+                </p>        
+            </div>
+
+            <div class="col-span-2 sm:flex sm:flex-row-reverse">
+                <div class="w-full text-center my-auto text-lg sm:px-8 sm:text-3xl text-gray-600 py-3">Climate change and sustainability management.</div>
+                <img class="max-h-96 w-full sm:w-1/2" src="/images/climate.jpg" alt="">
+            </div>
+
+            <div class="col-span-2 sm:flex">
+                <div class="text-center my-auto text-lg sm:px-8 sm:text-3xl text-gray-600 py-3">Agricultural value chain development and upward mobility.</div>            
+                <img class="max-h-96 w-full sm:w-1/2" src="/images/agric.jpg" alt="">
+            </div>
+
+            <div class="col-span-2 sm:flex sm:flex-row-reverse">
+                <div class="text-center my-auto text-lg sm:px-8 sm:text-3xl text-gray-600 py-3">Food sustainability and cyclical economy.</div>            
+                <img class="max-h-96 w-full sm:w-1/2" src="/images/food.jpg" alt="">
+            </div>
+                    
+            <div class="col-span-2 sm:flex">
+                <div class="text-center my-auto text-lg sm:px-8 sm:text-3xl text-gray-600 py-3">Predominantly solving social problems at the grassroot using diverse approaches that is tailored
+                    to specific demography.
+                </div> 
+                <img class="max-h-96 w-full sm:w-1/2" src="/images/agric1.jpg" alt="">
+            </div>
+
+        </div>
     </main-layout>
 </template>
 
@@ -8,18 +41,18 @@
 import MainLayout from "../layouts/MainLayout.vue";
 
 export default {
-  name: 'Index',
-  components: {
-      MainLayout,
-  },
-  props: {
+    name: 'Index',
+    components: {
+        MainLayout,
+    },
+    props: {
 
-  },
-  data() {
-    return {
+    },
+    data() {
+        return {
 
+        }
     }
-  }
 }
 </script>
 
